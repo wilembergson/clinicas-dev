e5cf9f81311afb08b40c59b22032ca1a726a33ff
wilembergson <wilembergson@yahoo.com>
2022-08-07 20:29:53 -0300

    Starting project

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export default function App() {
+    return (
+      <AppBody className="App">
+        Login 2
+      </AppBody>
+    )
+}
+
+const AppBody = styled.main`
+    display: flex;
+    justify-content: center;
+`
