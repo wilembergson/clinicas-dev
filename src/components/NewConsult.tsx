208d1f59ddf228bb1d86000a7b36699f267c5f52
wilembergson <wilembergson@yahoo.com>
2022-08-13 03:17:08 -0300

    Save new Consult

--- src/components/NewConsult.tsx
+++ src/components/NewConsult.tsx
@@ -3,8 +3,11 @@ import Select from "react-select"
 
 import { ButtonsContainer, Cancel, Confirm, Form, Input, Label } from "./SaredStyles"
 import { useContext, useEffect, useState } from "react"
-import api from "../api/ApiConections"
 import UserContext from "../contexts/UserContext"
+import { erroMessage, sucessMessage } from "../utils/toasts"
+import api from "../api/ApiConections"
+import { ToastContainer } from "react-toastify"
+import Loading from "./Loading"
 
 type SpecialtyItem = {
     id:number,
@@ -18,14 +21,32 @@ type SelectItem = {
 
 export default function NewConsult(){
     const { setPrincipalContentTitle } = useContext(UserContext)
+    const [loading, setLoading] = useState(false)
     const [specialtyList, setSpecialtylList] = useState<SelectItem[]>([])
-    const [specialty, setSpecialty] = useState<string | undefined>('')
+    const [specialtyName, setSpecialtyName] = useState<string | undefined>('')
     const [date, setDate] = useState('')
 
     function toMountSpetialtiesList(list:SpecialtyItem[]){
         const mountList:any = []
         list.forEach(item => mountList.push({ value: item.name, label: item.name }))
         setSpecialtylList(mountList)
+    }
+
+    async function handleSubmit(e:any){
+        e.preventDefault()
+        setLoading(true)
+        const promise = api.newConsult({specialtyName, date})
+        promise.then(response => {
+            sucessMessage(response.data.message)
+            setTimeout(()=> {
+                setPrincipalContentTitle('início')
+            }, 3000)
+            
+        })
+        .catch(error => {
+            setLoading(false)
+            erroMessage(error.response.data.error)
+        })
     }
 
     useEffect(() => {
@@ -35,26 +56,29 @@ export default function NewConsult(){
 
     return(
         <ConsultBody>
-            <Form>
+            <ToastContainer/>
+            <Form onSubmit={handleSubmit}>
                 <Label>ESPECIALIDADE</Label>
                 <Select
+                    isDisabled={loading}
                     options={specialtyList}
                     onChange={(e)=> {
                         const value = e?.value
-                        if(value) setSpecialty(value)
+                        if(value) setSpecialtyName(value)
                         }
                     }
                 />
                 <Label>DATA</Label>
                 <Input
+                    disabled={loading}
                     type="date"
                     onChange={(e) => setDate(e.target.value)}
                     name="Date"
                     required
                 />
                 <ButtonsContainer>
-                    <Confirm type="submit">Salvar</Confirm>
-                    <Cancel type="button" onClick={()=> setPrincipalContentTitle('início')}>Cancelar</Cancel>
+                    <Confirm type="submit" disabled={loading}>Salvar</Confirm>
+                    <Cancel type="button" disabled={loading} onClick={()=> setPrincipalContentTitle('início')}>Cancelar</Cancel>
                 </ButtonsContainer>
             </Form>
         </ConsultBody>
