9246d986166a6ee5a849d547aab794c0e321f0a4
wilembergson <wilembergson@yahoo.com>
2022-09-15 23:09:39 -0300

    Inital screen

--- src/pages/InitialPage.tsx
+++ src/pages/InitialPage.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
 
 import ImgBaner from "../assets/initialBaner.jpg"
+import Logo  from "../assets/logo-small.png"
 import { colors } from "../utils/Colors"
 
 export default function InitialPage(){
@@ -9,13 +10,13 @@ export default function InitialPage(){
     return(
         <>
             <InitialBanner>
-                <BannerTitle>Clínicas Dev</BannerTitle>
-                <BannerContent/>
-                <BannerImage src={ImgBaner}/>
+                <BannerImage src={Logo}/>
+                <BannerTitle>Agende suas consultas</BannerTitle>
+                <BannerTitle>sem sair de casa</BannerTitle>
             </InitialBanner>
             <ButtonsContainer>
-                <button onClick={() => navigate('/login')}>Login</button>
-                <button onClick={() => navigate('/register')}>Cadastre-se</button>
+                <BannerButton onClick={() => navigate('/login')}>Login</BannerButton>
+                <BannerButton onClick={() => navigate('/register')}>Cadastre-se</BannerButton>
             </ButtonsContainer>
         </>
     )
@@ -23,41 +24,54 @@ export default function InitialPage(){
 
 const InitialBanner = styled.div`
     display: flex;
-    justify-content: center;
+    align-items: center;
+    flex-direction: column;
     width: 100%;
     height: 65vh;
-    color: #FFF;
-    background: #000;
-    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
     overflow: hidden;
     position: relative;
+    color: ${colors.secondary};
+    background: rgba(228, 107, 107, 0.2);
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(5px);
+    -webkit-backdrop-filter: blur(5px);
 `
 const BannerImage = styled.img`
-    width: 100%;
-    height: 100vh;
-    position: absolute;
-    z-index: 1;
+    width: 300px;
+    height: 80px;
+    margin-top: 25px;
+    margin-bottom: 100px;
 `
-const BannerContent = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    position: relative;
-    z-index: 2;
-    background: #000;
-    opacity: 0.7;
+const BannerButton = styled.button`
+    width: 200px;
+    height: 70px;
+    color: #FFF;
+    background: ${colors.primary};
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    border: none;
+    border-radius: 40px;
+    font-size: 20px;
+    transition: 0.7s ease;
+    :hover{
+        background: ${colors.secondary};
+        transition: 0.7s ease;
+        transform: translateY(-10px);
+        transform: scale(1.1);
+    }
 `
 const BannerTitle = styled.label`
     background: none;
-    margin-top: 100px;
-    position: absolute;
     z-index: 3;
     font-family: 'Oxygen', sans-serif;
     font-size: 80px;
+    font-weight: 800;
 `
 const ButtonsContainer = styled.section`
     display: flex;
+    justify-content: space-around;
+    width: 100%;
     z-index: 2;
-    margin-top: -10px;
+    margin-top: -35px;
 `
